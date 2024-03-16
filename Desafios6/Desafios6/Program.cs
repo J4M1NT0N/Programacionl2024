2e259ce31da8dce4038b8e1115b0b4210188c03c
J4M1NT0N <jlopezl109@miumg.edu.gt>
2024-03-15 23:55:20 -0600

    Cree la carpeta desafios6

---
+++ Desafios6/Desafios6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Rectangulo
+{
+    private readonly double Alto;
+    private readonly double Largo;
+    private readonly double SuperficieFrontal;
+
+    public Rectangulo(double alto, double largo)
+    {
+        Alto = alto;
+        Largo = largo;
+        SuperficieFrontal = Alto * Largo;
+    }
+
+    public double ObtenerSuperficieFrontal()
+    {
+        return SuperficieFrontal;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangulo miRectangulo = new Rectangulo(5, 10);
+
+        Console.WriteLine("La superficie frontal del rectángulo es: " + miRectangulo.ObtenerSuperficieFrontal());
+    }
+}
