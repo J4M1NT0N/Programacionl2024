664e577761de3e4035012137aa9609734790df0b
J4M1NT0N <jlopezl109@miumg.edu.gt>
2024-04-12 19:40:32 -0600

    Subir desafios de la semana 9

---
+++ DesafioSemana9/DesafioSemana9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Por favor ingrese un texto:");
+        string texto = Console.ReadLine();
+
+        string patron = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+
+        Regex regex = new Regex(patron);
+
+        MatchCollection coincidencias = regex.Matches(texto);
+
+        Console.WriteLine("Estas son las direcciones de correo electrónico encontradas:");
+
+        foreach (Match coincidencia in coincidencias)
+        {
+            Console.WriteLine(coincidencia.Value);
+        }
+    }
+}
