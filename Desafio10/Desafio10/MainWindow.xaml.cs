e03d1471220b39a76627b7eebf002b5e12df0344
J4M1NT0N <jlopezl109@miumg.edu.gt>
2024-05-17 23:48:39 -0600

    Subida de proyecto y desafio

---
+++ Desafio10/Desafio10/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Desafio10;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Desafio10
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeJuegos();
+        }
+
+        private void InitializeJuegos()
+        {
+            List<Juego> juegos = new List<Juego>();
+
+            // Ruta del archivo de texto
+            string filePath = "C:\\Users\\LENOVO\\Desktop\\Git Hub\\Programacionl2024\\Desafio10\\Desafio10\\textojuego.txt";
+
+            // Leer todas las líneas del archivo
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Procesar cada línea para crear un objeto Juego
+            foreach (string line in lines)
+            {
+                string[] datos = line.Split(',');
+                if (datos.Length == 5)
+                {
+                    Juego juego = new Juego()
+                    {
+                        Eq1 = datos[0],
+                        Eq2 = datos[1],
+                        Puntaje1 = Convert.ToInt32(datos[2]),
+                        Puntaje2 = Convert.ToInt32(datos[3]),
+                        Progreso = Convert.ToInt32(datos[4])
+                    };
+                    juegos.Add(juego);
+                }
+            }
+
+            // Asignar la lista de juegos al ListBox
+            lbJuego.ItemsSource = juegos;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbJuego.SelectedItem != null)
+            {
+                MessageBox.Show("Juego Seleccionado: " + (lbJuego.SelectedItem as Juego).Eq1 + " " +
+                (lbJuego.SelectedItem as Juego).Puntaje1 + " " + (lbJuego.SelectedItem as Juego).Eq2 + " " + (lbJuego.SelectedItem as Juego).Puntaje2);
+            }
+        }
+    }
+}
---
+++ Proyecto Final/Proyecto Final/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_Final
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Transaccion> transacciones = new ObservableCollection<Transaccion>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dgTransacciones.ItemsSource = transacciones;
+        }
+
+        private void cmbTipoTransaccion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem selectedItem = cmbTipoTransaccion.SelectedItem as ComboBoxItem;
+            if (selectedItem != null)
+            {
+                string tipoTransaccion = selectedItem.Content.ToString();
+                lblCantidad.Visibility = tipoTransaccion == "Compra" || tipoTransaccion == "Venta" ? Visibility.Visible : Visibility.Collapsed;
+                txtCantidad.Visibility = tipoTransaccion == "Compra" || tipoTransaccion == "Venta" ? Visibility.Visible : Visibility.Collapsed;
+                txtTipoMoneda.Visibility = tipoTransaccion == "Compra" || tipoTransaccion == "Venta" ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        private void RegistrarTransaccion_Click(object sender, RoutedEventArgs e)
+        {
+            string tipo = (cmbTipoTransaccion.SelectedItem as ComboBoxItem)?.Content.ToString();
+            if (string.IsNullOrEmpty(tipo))
+            {
+                MessageBox.Show("Por favor, seleccione un tipo de transacción.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string descripcion = txtDescripcion.Text;
+            decimal monto;
+            decimal cantidad;
+
+            if (!decimal.TryParse(txtMonto.Text, out monto))
+            {
+                MessageBox.Show("Por favor, ingrese un monto válido.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!decimal.TryParse(txtCantidad.Text, out cantidad) || cantidad <= 0)
+            {
+                MessageBox.Show("Por favor, ingrese una cantidad válida para la transacción.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            string tipoMoneda = txtTipoMoneda.Text;
+
+            transacciones.Add(new Transaccion { Tipo = tipo, Descripcion = descripcion, Monto = monto, Cantidad = cantidad, TipoMoneda = tipoMoneda });
+
+            txtDescripcion.Clear();
+            txtMonto.Clear();
+            txtCantidad.Clear();
+            txtTipoMoneda.Text = "Q";
+        }
+
+
+
+    }
+}
